914f4734e37c22da63e57a38c575e53e8452d81d
Julie Judachina <iuliia.saunova@email.cz>
2021-07-27 00:31:18 +0200

    fetchimage

---
+++ src/js/fetchImages.js
@@ -0,0 +1,34 @@
+import Notiflix from "notiflix";
+const API_URL = 'https://pixabay.com/api/';
+const API_KEY = '22647650-093efc913fe4b5bfd764725e8';
+
+export default class ImageService {
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+
+    };
+
+    async fetchImages() {
+        const url = `${API_URL}?key = ${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true& per_page=40&page=${this.page}`
+        const response = await axios.get(url);
+        this.incrementPage()
+        return response;
+    }
+
+    incrementPage() {
+        this.page += 1;
+    }
+
+    resetPage() {
+        this.page = 1;
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+    set query(newQuery) {
+        this.searchQuery = newQuery;
+    }
+}
