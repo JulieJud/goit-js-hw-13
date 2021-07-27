23b322a496106a4c354ec6373e7e6f2b73b322cb
Julie Judachina <iuliia.saunova@email.cz>
2021-07-27 02:09:06 +0200

    add lightbox

--- src/index.js
+++ src/index.js
@@ -5,3 +5,64 @@ import ImageService from './js/fetchImages'
 import imagesTpl from './templates/card.hbs'
 
 
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    imagesContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more')
+}
+
+const lightbox = new SimpleLightbox('.photo-card a');
+refs.searchForm.addEventListener('submit', onSearch)
+refs.loadMoreBtn.addEventListener('click', onloadMoreBtnClick)
+
+const newApi = new ImageService();
+
+
+async function onSearch(e) {
+    e.preventDefault();
+
+    newApi.query = e.currentTarget.elements.searchQuery.value;
+
+    if (newApi.query === '') {
+        return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+
+    newApi.resetPage();
+
+    const response = await newApi.fetchImages();
+
+    clearImageContainer();
+
+    return await imageMarkup(response);
+}
+
+async function onloadMoreBtnClick() {
+    const response = await newApi.fetchImages();
+    lightbox.refresh()
+    return imageMarkup(response);
+
+}
+
+
+
+function imageMarkup(images) {
+    refs.imagesContainer.insertAdjacentHTML('beforeend', imagesTpl(images))
+    lightbox.refresh()
+    refs.loadMoreBtn.classList.remove('is-hidden')
+
+    if (images.length === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden')
+        return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+
+
+
+    if (images.length < 40) {
+        refs.loadMoreBtn.classList.add('is-hidden')
+        Notiflix.Notify.info('We are sorry, but you have reached the end of search results.');
+    }
+}
+
+function clearImageContainer() {
+    refs.imagesContainer.innerHTML = '';
+}
--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Notiflix from "notiflix";
 const API_URL = 'https://pixabay.com/api/';
 const API_KEY = '22647650-093efc913fe4b5bfd764725e8';
 
@@ -9,18 +10,18 @@ export default class ImageService {
 
     };
 
-    async fetchImages() {
-        const url = `${API_URL}?key = ${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true& per_page=40&page=${this.page}`
-        const response = await axios.get(url);
-        this.incrementPage()
-        return response;
+    fetchImages() {
+        return fetch(`${API_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
+            .then(response => response.json())
+            .then(({ total, hits }) => {
+                if (total > 0 && this.page === 1) {
+                    Notiflix.Notify.success(`Hooray! We found ${total} images.`);
+                }
+                this.page += 1;
+                return hits;
+            })
     }
 
-
-
-    incrementPage() {
-        this.page += 1;
-    }
 
     resetPage() {
         this.page = 1;
